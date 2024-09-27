cf8aa499039d912f63c6c337884e7a513c15cc90
Diana050983 <obice20@gmail.com>
2024-09-27 16:35:50 -0500

    cambio 2

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,9 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+            System.out.println("i * i = "+i*i);
         }
+        System.out.println("Hola desde el main");
+        System.out.println("Hola  2 desde el main");
     }
 }
